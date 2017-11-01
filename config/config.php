7e020cdc27a359ed30745587ee52d394e6d58b31
rik471 <riksongleison471@gmail.com>
2017-11-01 21:18:52 -0200

    f

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-
-  $_SESSION["nome"] = "Rick";
-  echo "ID de Sessão: ". session_id() . "<br>";
-  ?>
--- json/teste-json.php
+++
@@ -1,7 +0,0 @@
-<p><h4>Testando json</h4><?php
-  //decodando arq json
-
-  $json = '[{"Death Metal":"Death"},{"Thrash Metal":"Pantera"}]';
-  $data = json_decode($json, true);
-  var_dump($data);
-?></p>
