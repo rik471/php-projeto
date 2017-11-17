d966e5ec99990e242ab7f25d65d8ff57f8728397
rik471 <riksongleison471@gmail.com>
2017-11-16 22:30:53 -0200

    ok

--- poo/Episodio1.php
+++ poo/Episodio1.php
@@ -4,6 +4,7 @@
 class Episodio1 extends StrangerThings {
 
   public function willthewise($serie){
+    $this->serie = $serie;
     echo "Neste Ep de ".$this->serie. ", Will está preso no corpo do monstro.";
   }
 }
