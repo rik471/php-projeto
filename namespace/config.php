687565ebaf76cc811c3d9b517e202a278f7d85aa
rik471 <riksongleison471@gmail.com>
2017-11-25 02:11:14 -0200

    fixing dependencies

--- namespace/config.php
+++ namespace/config.php
@@ -1,13 +1,17 @@
 <?php
-    spl_autoload_register(function($nameClass){
+    spl_autoload_register(function($nameClass){//solicitando arquivo da classe
 
         $dirClass = "class";//pasta da classe
-        $filename = $dirClass . DIRECTORY_SEPARATOR . $nameClass .".php";
+
+        $filename = str_replace ("\\", "/", $dirClass . DIRECTORY_SEPARATOR . $nameClass .".php");
         //separando por diretorio e pegando a classe de cada pasta e subpasta
 
         if(file_exists($filename)) {
 
           require_once($filename);
+        }else {
+          echo "Arquivo não encontrado.<br>Verifique se o nome do arquivo
+          é o mesmo da classe...<br>";
         }
 
   });
